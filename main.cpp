12fded8bd94699fe6263407f9eb7023dce9b1940
axel-ribes <axel.ribes@etu.univ-amu.fr>
2020-02-19 17:29:48 +0000

    stuck at the end of the astar algorithm, cannot figure out how to sort the array

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include "FileReader.h"
+#include "Graph.h"
+#include "Node.h"
 
 using namespace std;
 
@@ -18,22 +21,16 @@ void displayFile(string file_name) {
 	ifs.close();
 }; //reading file and display it
 
-void createGraph(string file_name) {
-	ifstream ifs(file_name);
-	string line = "";
-	while (getline(ifs, line))
-	{
-		//processing to create : nodes, edges --> typedef std::pair<int, int> edge;
-		//graph is composed of vector<node> and vector<edge>
-	}
-	ifs.close();
-}; //setting up a graph so we can process with astar
-
-void findPath(/*graph*/) {
-	//use astar to find path 
-}
 
 void main() {
-	
-	displayFile("test.txt");
+	string fileName = "random64_4_1485816605.dot"; 
+	FileReader* f = new FileReader(fileName); //creating the fileReader component
+	Graph graph = f->readFile(); //parsing the file to create and fill the graph
+	std::vector<Node> path = graph.searchPathAstar(graph.getListOfNodes()->at(0), graph.getListOfNodes()->at(60));
+
+	for (unsigned i = 0; i < path.size(); ++i)
+		std::cout << "Pass through: " << path[i].getID() << std::endl;
+
+	//cout << graph.getListOfNodes();
+	//displayFile("random64_4_1485816605.dot");
 }
