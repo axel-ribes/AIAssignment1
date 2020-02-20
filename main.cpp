bbc0e363b5e4451700ac855f59372d618e0d076f
axel-ribes <axel.ribes@etu.univ-amu.fr>
2020-02-20 18:16:09 +0000

    Added the pdf report and last changes to the code (manually enterring the nodes)

--- main.cpp
+++ main.cpp
@@ -26,10 +26,36 @@ void main() {
 	string fileName = "random64_4_1485816605.dot"; 
 	FileReader* f = new FileReader(fileName); //creating the fileReader component
 	Graph graph = f->readFile(); //parsing the file to create and fill the graph
-	std::vector<Node> path = graph.searchPathAstar(graph.getListOfNodes()->at(0), graph.getListOfNodes()->at(60));
+	int start,end;
+	start = end = 0;
+	
+	cout << "Please enter the start node : " << endl;
+	cin >> start;
+	while (!cin || start > 64 || start < 0)
+	{
+		cout << "Please enter a valid Node Between 0 and 63 :" << endl;
+		cin.clear();
+		cin.ignore();
+		cin >> start;
+	}
+
+	cout << endl << "Please enter the end node : " << endl;
+	cin >> end;
+
+	while (!cin || end > 64 || end < 0 || end == start)
+	{
+		cout << "Please enter a valid Node Between 0 and 63 and not the same as the start :" << endl;
+		cin.clear();
+		cin.ignore();
+		cin >> end;
+	}
+
+	cout << endl << "Processing..." << endl << endl;
+
+	std::vector<Node> path = graph.searchPathAstar(graph.getListOfNodes()->at(start), graph.getListOfNodes()->at(end));
 
 	for (unsigned i = 0; i < path.size(); ++i)
-		std::cout << "Pass through: " << path[i].getID() << std::endl;
+		std::cout << "Node : " << path[i].getID() << std::endl;
 
 	//cout << graph.getListOfNodes();
 	//displayFile("random64_4_1485816605.dot");
